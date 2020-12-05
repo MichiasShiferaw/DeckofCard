1009aa9f770ffa2243baf26b05d935006187ac7d
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-05 18:31:48 -0500

    Add sets of options(suits and ranks)

--- DeckOfCard.py
+++ DeckOfCard.py
@@ -17,6 +17,8 @@ class Card:
 
 
 class Deck:
+    suits={'\u2660','\u2663','\u2665','\u2666'}
+    ranks={"2","3","4","5","6","7","8","9","10","J","Q","K","A"}
     def __init__(self):
         self.deck=[]
         
